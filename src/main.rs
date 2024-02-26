fc8858a343abf1af1e0bfccf9f23f0b997913c7e
7ejasVijay <7ejas.vijay@gmail.com>
2024-02-26 19:10:48 +0530

    refactor cat and echo options

--- src/main.rs
+++ src/main.rs
@@ -4,24 +4,33 @@ use std::fs;
 fn main() {
     let cli = Cli::parse();
 
-    cli.cat();
+    if let Some(echo) = cli.echo.as_deref() {
+        echo_display(echo);
+    }
+    if let Some(cat) = cli.cat.as_deref() {
+        cat_display(cat);
+    }
 }
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
 struct Cli {
+    /// Print the text to the standard output
     #[arg(long)]
     echo: Option<String>,
+    /// Print the text from a file to standard output
     #[arg(long)]
-    cat: String,
+    cat: Option<String>,
 }
 
-impl Cli {
-    // fn echo(&self) {
-    //     println!("{}", self.echo.unwrap());
-    // }
-    fn cat(&self) {
-        let contents = fs::read_to_string(&self.cat).expect("The file cannot be opened!");
-        println!("{contents}");
-    }
+fn echo_display(text: &str) {
+    println!("{text}");
+}
+
+fn cat_display(path: &str) {
+    let contents = match fs::read_to_string(path) {
+        Ok(content) => content,
+        Err(_) => String::from("Error opening the file!"),
+    };
+    println!("{contents}");
 }
